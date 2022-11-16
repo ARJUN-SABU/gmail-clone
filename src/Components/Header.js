ee0a7939f23dd6323e009d24c14213eaeab5e2d5
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-16 13:06:21 +0530

    Added Search bar and other gmail icons on the right of the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,9 @@
 //icons
 import { MdMenu } from "react-icons/md";
 import { MdSearch } from "react-icons/md";
+import { IoMdOptions } from "react-icons/io";
+import { IoSettingsOutline, IoApps } from "react-icons/io5";
+import { FaUserCircle } from "react-icons/fa";
 
 //styles
 import "../styles/Header.css";
@@ -15,10 +18,25 @@ function Header() {
         <img src="gmail_icon.png" alt="gmail_icon" />
       </div>
       <div className="header__middle">
-        <MdSearch />
+        <div className="header__middle__iconContainer">
+          <MdSearch size={"24px"} />
+        </div>
         <input type="text" placeholder="Search mail" />
+        <div className="header__middle__iconContainer">
+          <IoMdOptions size={"24px"} />
+        </div>
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <div className="header__right__imageContainer">
+          <IoSettingsOutline size={"20px"} />
+        </div>
+        <div className="header__right__imageContainer">
+          <IoApps size={"20px"} />
+        </div>
+        <div className="header__right__avatarContainer">
+          <FaUserCircle size={"30px"} />
+        </div>
+      </div>
     </div>
   );
 }
