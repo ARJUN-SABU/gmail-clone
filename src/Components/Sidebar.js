547dc1e5ab68c7dd783a67c3378e496bacc411e8
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-20 20:46:41 +0530

    Added button to the sidebar component

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,10 +1,18 @@
+//icons
+import { RiPencilFill } from "react-icons/ri";
+
 //styles
 import "../styles/Sidebar.css";
 
 function Sidebar() {
   return (
-    <div>
-      <h1>This is the sidebar</h1>
+    <div className="sidebar">
+      <button className="sidebar__composeBtn">
+        <span>
+          <RiPencilFill size={"23px"} />
+        </span>
+        <span>Compose</span>
+      </button>
     </div>
   );
 }
