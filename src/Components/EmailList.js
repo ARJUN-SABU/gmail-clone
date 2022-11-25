110a309f809099118718b1757b18c06df3bef2f2
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-25 19:25:07 +0530

    Gave basic outline and shape to the middle area of emails-list

--- src/Components/EmailList.js
+++ src/Components/EmailList.js
@@ -16,25 +16,25 @@ function EmailList() {
       <div className="emailList__settings">
         <div className="emailList__settings__left">
           <div className="emailList__setting__iconContainer emailList__setting__left__iconContainer emailList__setting__left__checkboxIcon">
-            <MdCheckBoxOutlineBlank size={"20px"} />
-            <AiFillCaretDown size={"12px"} />
+            <MdCheckBoxOutlineBlank size={"18px"} />
+            <AiFillCaretDown size={"11px"} />
           </div>
 
           <div className="emailList__setting__iconContainer emailList__setting__left__iconContainer">
-            <MdRefresh size={"20px"} />
+            <MdRefresh size={"18px"} />
           </div>
 
           <div className="emailList__setting__iconContainer emailList__setting__left__iconContainer">
-            <MdMoreVert size={"20px"} />
+            <MdMoreVert size={"18px"} />
           </div>
         </div>
         <div className="emailList__settings__right">
           <div className="emailList__setting__iconContainer emailList__setting__right__iconContainer">
-            <AiOutlineLeft />
+            <AiOutlineLeft size={"13px"} />
           </div>
 
           <div className="emailList__setting__iconContainer emailList__setting__right__iconContainer">
-            <AiOutlineRight />
+            <AiOutlineRight size={"13px"} />
           </div>
         </div>
       </div>
