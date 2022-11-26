d08f50f9085dcfc382a1a63641ee0f4b3916b47a
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-26 20:32:45 +0530

    Added the left 3 icons of emailRow

--- src/Components/EmailRow.js
+++ src/Components/EmailRow.js
@@ -1,4 +1,9 @@
 //icons
+import { AiOutlineStar } from "react-icons/ai";
+import {
+  MdLabelImportantOutline,
+  MdCheckBoxOutlineBlank,
+} from "react-icons/md";
 
 //styles
 import "../styles/EmailRow.css";
@@ -6,7 +11,17 @@ import "../styles/EmailRow.css";
 function EmailRow(props) {
   return (
     <div className="emailRow">
-      <div className="emailRow__left"></div>
+      <div className="emailRow__left">
+        <div className="emailRow__icons">
+          <MdCheckBoxOutlineBlank />
+        </div>
+        <div className="emailRow__icons">
+          <AiOutlineStar />
+        </div>
+        <div className="emailRow__icons">
+          <MdLabelImportantOutline />
+        </div>
+      </div>
       <div className="emailRow__middle"></div>
       <div className="emailRow__right"></div>
     </div>
