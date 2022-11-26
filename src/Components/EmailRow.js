dfd2195badda01b80d1bd705376f3b37ecef42a1
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-26 20:26:01 +0530

    Created the basic .js file and .css file for EmailRow

---
+++ src/Components/EmailRow.js
@@ -0,0 +1,16 @@
+//icons
+
+//styles
+import "../styles/EmailRow.css";
+
+function EmailRow(props) {
+  return (
+    <div className="emailRow">
+      <div className="emailRow__left"></div>
+      <div className="emailRow__middle"></div>
+      <div className="emailRow__right"></div>
+    </div>
+  );
+}
+
+export default EmailRow;
