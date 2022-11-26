f9930e90e1760f4b5dd0a0ba7c875a81a6a56600
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-26 20:43:24 +0530

    Added The right side icons and fileds for title, subject and time in the email row

--- src/Components/EmailRow.js
+++ src/Components/EmailRow.js
@@ -1,12 +1,16 @@
 //icons
-import { AiOutlineStar } from "react-icons/ai";
+import { AiOutlineStar, AiOutlineClockCircle } from "react-icons/ai";
 import {
   MdLabelImportantOutline,
   MdCheckBoxOutlineBlank,
+  MdDeleteOutline,
+  MdMailOutline,
 } from "react-icons/md";
+import { BiArchiveIn } from "react-icons/bi";
 
 //styles
 import "../styles/EmailRow.css";
+import { CgLock } from "react-icons/cg";
 
 function EmailRow(props) {
   return (
@@ -21,9 +25,29 @@ function EmailRow(props) {
         <div className="emailRow__icons">
           <MdLabelImportantOutline />
         </div>
+        <p>{props.title}</p>
       </div>
-      <div className="emailRow__middle"></div>
-      <div className="emailRow__right"></div>
+      <div className="emailRow__middle">
+        <p>{props.subject}</p>
+        <p>{props.description}</p>
+      </div>
+      <div className="emailRow__right">
+        <p>{props.time}</p>
+      </div>
+      <div className="emailRow__rightOptions">
+        <div className="emailRow__icons">
+          <BiArchiveIn />
+        </div>
+        <div className="emailRow__icons">
+          <MdDeleteOutline />
+        </div>
+        <div className="emailRow__icons">
+          <MdMailOutline />
+        </div>
+        <div className="emailRow__icons">
+          <AiOutlineClockCircle />
+        </div>
+      </div>
     </div>
   );
 }
