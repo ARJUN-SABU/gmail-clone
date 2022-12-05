a8708eff68f3d503ed9b8599f5c79c5d99066a2a
ARJUN-SABU <arjunsabu99@gmail.com>
2022-12-05 18:26:15 +0530

    Added hide and show feature for email row right options

--- src/Components/EmailRow.js
+++ src/Components/EmailRow.js
@@ -13,8 +13,32 @@ import "../styles/EmailRow.css";
 import { CgLock } from "react-icons/cg";
 
 function EmailRow(props) {
+  function showEmailRowRightOptions(event) {
+    event.target
+      .querySelector(".emailRow__rightOptions")
+      .classList.remove("emailRow__rightOptions--hide");
+
+    event.target
+      .querySelectorAll(".emailRow__icons")
+      .forEach((emailRow) => emailRow.classList.add("emailRow__icon--bold"));
+  }
+
+  function hideEmailRowRightOptions(event) {
+    event.target
+      .querySelector(".emailRow__rightOptions")
+      .classList.add("emailRow__rightOptions--hide");
+
+    event.target
+      .querySelectorAll(".emailRow__icons")
+      .forEach((emailRow) => emailRow.classList.remove("emailRow__icon--bold"));
+  }
+
   return (
-    <div className="emailRow">
+    <div
+      className="emailRow"
+      onMouseEnter={showEmailRowRightOptions}
+      onMouseLeave={hideEmailRowRightOptions}
+    >
       <div className="emailRow__left">
         <div className="emailRow__icons">
           <MdCheckBoxOutlineBlank size={"18px"} />
@@ -36,18 +60,18 @@ function EmailRow(props) {
       <div className="emailRow__right">
         <p className="emailRow__time">{props.time}</p>
       </div>
-      <div className="emailRow__rightOptions">
+      <div className="emailRow__rightOptions emailRow__rightOptions--hide">
         <div className="emailRow__icons">
-          <BiArchiveIn />
+          <BiArchiveIn size={"18px"} />
         </div>
         <div className="emailRow__icons">
-          <MdDeleteOutline />
+          <MdDeleteOutline size={"18px"} />
         </div>
         <div className="emailRow__icons">
-          <MdMailOutline />
+          <MdMailOutline size={"18px"} />
         </div>
         <div className="emailRow__icons">
-          <AiOutlineClockCircle />
+          <AiOutlineClockCircle size={"18px"} />
         </div>
       </div>
     </div>
